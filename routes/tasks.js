d9ccaa6bce6543101b4177df192bfa291f609f57
sharad choudhary <sharadc983@gmail.com>
2026-04-11 14:18:57 +0530

    setup for frontend with react

--- routes/tasks.js
+++ routes/tasks.js
@@ -3,6 +3,7 @@ const express=require("express")
 const TaskModel=require("../models/tasks")
 const userAuth=require("../middleware/user")
 const validateTasks=require("../helper/tasks")
+const mongoose=require("mongoose")
 
 
 const router=express.Router();
@@ -77,8 +78,107 @@ router.get("/",userAuth,async(req,res)=>{
     }
 })
 
+//update a task
+router.patch("/:id",userAuth,async(req,res)=>{
+
+    try{
+        
+      const allowedUpdates = [
+      "title",
+      "description",
+      "status",
+      "priority",
+      "dueDate"
+    ];
+
+    //perform validtaion on task id
+    if(!mongoose.Types.ObjectId.isValid(req.params.id)){
+        return res.status(400).json({
+            success:false,
+            message:"Invalid task Id"
+        })
+    }
+   
+    //return array of object keys
+    const updates=Object.keys(req.body);
+
+    const isValid=updates.every( (field)=>{
+      return  allowedUpdates.includes(field)
+    })
+    
+    if(!isValid){
+     return res.status(400).json({
+           
+        success:false,
+        message:"Invalid Updates"
+        })
+    }
+    
+    const task =await TaskModel.findOne({
+     _id:req.params.id,
+     userId:req.user._id
+    })
+
+    if(!task){
+        return res.status(404).json({
+            success:false,
+            message:"Task not found"
+        })
+    }
+
+    const updatedTask=await TaskModel.findOneAndUpdate({_id:req.params.id,userId:req.user._id},req.body,{new:true,runvalidators:true})
+
+    res.status(200).json({
+        success:true,
+        message:"task updated successfully",
+        task:updatedTask
+    })
 
 
+    }catch(err){
+        
+        res.status(500).json({
+            success:false,
+            message:err.message
+        })
+
+    }
+
+})
+
+
+//delete a task
+router.delete("/:id",userAuth,async(req,res)=>{
+
+    try{
+            
+        //perform validation on task id because it prevent from cast error which is given by server
+
+        if(!mongoose.Types.ObjectId.isValid(req.params.id)){
+            return res.status(400).json({
+                success:false,
+                message:"Invalid task id"
+            })
+        }
+        
+
+        const deletedTask=await TaskModel.findOneAndDelete({_id:req.params.id,userId:req.user._id})
+
+        res.status(200).json({
+            success:true,
+            message:"task deleted successfully"
+        })
+        
+
+    }catch(err){
+       
+        console.log(err.message)
+        res.status(500).json({
+            success:false,
+            message:err.message
+        })
+    }
+})
 
 
 module.exports=router;
